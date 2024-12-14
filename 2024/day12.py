6806180ee24fcbed55dd97879b77459158a96556
Michal Gabriel <mgabriel@artex-is.cz>
2024-12-14 14:03:47 +0100

    day12 cleanup + config/small fixes

--- 2024/day12.py
+++ 2024/day12.py
@@ -7,43 +7,24 @@ import collections
 import time
 
 
-def expand_marks(field, m, pos):
+def expand_marks(m, pos):
     queue = [pos]
     while len(queue) != 0:
         p = queue.pop(0)
-        if field[p[0]][p[1]] == 0:
-            field[p[0]][p[1]] = offsets[m]
-            if (p[1]-1 >= 0) and (0 == field[p[0]][p[1]-1]) and (lin[p[0]][p[1]-1] == m):
+        if marks[p[0]][p[1]] is None:
+            marks[p[0]][p[1]] = (m, offsets[m])
+            if (p[1]-1 >= 0) and (marks[p[0]][p[1]-1] is None) and (lin[p[0]][p[1]-1] == m):
                 queue.append((p[0], p[1]-1))
-            if (p[1]+1 < len(lin[0])) and (0 == field[p[0]][p[1]+1]) and (lin[p[0]][p[1]+1] == m):
+            if (p[1]+1 < len(lin[0])) and (marks[p[0]][p[1]+1] is None) and (lin[p[0]][p[1]+1] == m):
                 queue.append((p[0], p[1]+1))
-            if (p[0]-1 >= 0) and (0 == field[p[0]-1][p[1]]) and (lin[p[0]-1][p[1]] == m):
+            if (p[0]-1 >= 0) and (marks[p[0]-1][p[1]] is None) and (lin[p[0]-1][p[1]] == m):
                 queue.append((p[0]-1, p[1]))
-            if (p[0]+1 < len(lin)) and (0 == field[p[0]+1][p[1]]) and (lin[p[0]+1][p[1]] == m):
+            if (p[0]+1 < len(lin)) and (marks[p[0]+1][p[1]] is None) and (lin[p[0]+1][p[1]] == m):
                 queue.append((p[0]+1, p[1]))
-
-
-def init_marks(m, pos):
-    field = [[0 for x in range(len(lin[0]))] for y in range(len(lin))]
-    expand_marks(field, m, pos)
-    marks[m] = field
 
 
 day = 12
 lin = [l.replace('\n', '') for l in util.get_input(day)]
-
-# lin = """RRRRIICCFF
-# RRRRIICCCF
-# VVRRRCCFFF
-# VVRCCCJFFF
-# VVVVCJJCFE
-# VVIVCCJJEE
-# VVIIICJJEE
-# MIIIIIJJEE
-# MIIISIJEEE
-# MMMISSJEEE
-# """.split('\n')
-# lin.pop(-1)
 
 acc = 0
 acc2 = 0
@@ -52,44 +33,39 @@ areas = collections.Counter()
 perimeters = collections.Counter()
 perimeters_2 = collections.Counter()
 offsets = collections.Counter()
-marks = dict()
 
 start = time.time()
 for y in range(len(lin)):
     lin[y] = '#'+lin[y]+'#'
 lin = ['#'*len(lin[0])]+lin+['#'*len(lin[0])]
-
+marks = [[None for x in range(len(lin[0]))] for y in range(len(lin))]
 for y in range(1, len(lin)-1):
     for x in range(1, len(lin[0])-1):
         region = lin[y][x]
-        if not (region in marks.keys()):
+        if marks[y][x] is None:
             offsets[region] += 1
-            init_marks(region, (y, x))
-        else:
-            if not marks[region][y][x]:
-                offsets[region] += 1
-                expand_marks(marks[region], region, (y, x))
-        areas[region+str(marks[region][y][x])] += 1
+            expand_marks(region, (y, x))
+        areas[str(marks[y][x])] += 1
         if lin[y][x-1] != region:
-            perimeters[region+str(marks[region][y][x])] += 1
-            perimeters_2[region+str(marks[region][y][x])] += 1
+            perimeters[str(marks[y][x])] += 1
+            perimeters_2[str(marks[y][x])] += 1
             if (lin[y-1][x-1] != region) and (lin[y-1][x] == region):
-                perimeters_2[region+str(marks[region][y][x])] -= 1
+                perimeters_2[str(marks[y][x])] -= 1
         if lin[y][x+1] != region:
-            perimeters[region+str(marks[region][y][x])] += 1
-            perimeters_2[region+str(marks[region][y][x])] += 1
+            perimeters[str(marks[y][x])] += 1
+            perimeters_2[str(marks[y][x])] += 1
             if (lin[y-1][x+1] != region) and (lin[y-1][x] == region):
-                perimeters_2[region+str(marks[region][y][x])] -= 1
+                perimeters_2[str(marks[y][x])] -= 1
         if lin[y-1][x] != region:
-            perimeters[region+str(marks[region][y][x])] += 1
-            perimeters_2[region+str(marks[region][y][x])] += 1
+            perimeters[str(marks[y][x])] += 1
+            perimeters_2[str(marks[y][x])] += 1
             if (lin[y-1][x-1] != region) and (lin[y][x-1] == region):
-                perimeters_2[region+str(marks[region][y][x])] -= 1
+                perimeters_2[str(marks[y][x])] -= 1
         if lin[y+1][x] != region:
-            perimeters[region+str(marks[region][y][x])] += 1
-            perimeters_2[region+str(marks[region][y][x])] += 1
+            perimeters[str(marks[y][x])] += 1
+            perimeters_2[str(marks[y][x])] += 1
             if (lin[y+1][x-1] != region) and (lin[y][x-1] == region):
-                perimeters_2[region+str(marks[region][y][x])] -= 1
+                perimeters_2[str(marks[y][x])] -= 1
 
 for k in areas.keys():
     acc += areas[k]*perimeters[k]
