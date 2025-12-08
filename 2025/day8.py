fab573368ac79ba193918dc7d8ace44c1265c354
Michal Gabriel <mgabriel@artex-is.cz>
2025-12-08 15:27:16 +0100

    fixing day 8

--- 2025/day8.py
+++ 2025/day8.py
@@ -17,7 +17,7 @@ def get_dist(box1, box2):
     return math.sqrt((int(b1[0])-int(b2[0]))**2+(int(b1[1])-int(b2[1]))**2+(int(b1[2])-int(b2[2]))**2)
 
 
-def part1():
+def part1_2():
     components = np.zeros(len(lin))
     component_id = 1
     dist_list = []
@@ -26,9 +26,9 @@ def part1():
             if n1 != n2:
                 dist_list.append(((n1, n2), get_dist(lin[n1], lin[n2])))
     dist_list.sort(key=lambda index: index[1])
-    for i in range(10):
-        print(lin[dist_list[i][0][0]] + ' - ' + lin[dist_list[i][0][1]])
-    target = 10
+    # for i in range(10):
+    #     print('('+str(dist_list[i][0][0])+')'+lin[dist_list[i][0][0]] + ' - ' + '('+str(dist_list[i][0][1])+')' + lin[dist_list[i][0][1]] + ' : '+str(dist_list[i][1]))
+    target = 1000
     iterations = 0
     d = 0
     while d < (min(target, len(dist_list))):
@@ -40,9 +40,7 @@ def part1():
                     components[n1], components[n2]))
                 for i in range(len(lin)):
                     if components[i] == replacement[1]:
-                        components[i] == replacement[0]
-            else:
-                target += 1
+                        components[i] = replacement[0]
         if (components[n1] == 0) and (components[n2] != 0):
             components[n1] = components[n2]
         if (components[n1] != 0) and (components[n2] == 0):
@@ -52,29 +50,49 @@ def part1():
             components[n2] = component_id
             component_id += 1
         d += 1
-    print(components)
-    print('iterations: '+str(iterations))
+    # print(components)
+    # print('iterations: '+str(iterations))
     c = collections.Counter(components)
     mc = c.most_common(4)
-    print(c)
+    # print(c)
     acc = 1
     for mc_entry in mc:
         if mc_entry[0] != 0:
             acc *= mc_entry[1]
+
+    print('part 1: '+str(acc))
+
+    while d < len(dist_list):
+        n1, n2 = dist_list[d][0]
+        if (components[n1] != 0) and (components[n2] != 0):
+            if components[n1] != components[n2]:
+                replacement = (min(components[n1], components[n2]), max(
+                    components[n1], components[n2]))
+                for i in range(len(lin)):
+                    if components[i] == replacement[1]:
+                        components[i] = replacement[0]
+        if (components[n1] == 0) and (components[n2] != 0):
+            components[n1] = components[n2]
+        if (components[n1] != 0) and (components[n2] == 0):
+            components[n2] = components[n1]
+        if (components[n1] == 0) and (components[n2] == 0):
+            components[n1] = component_id
+            components[n2] = component_id
+            component_id += 1
+        if components.sum() == len(lin):
+            # print(lin[n1] + ' - ' + lin[n2])
+            # print('components: '+str(components))
+            print('part 2: '+str(int(lin[n1].split(',')[0])*int(lin[n2].split(',')[0])))
+            break
+        d += 1
+
     return acc
-
-
-def part2():
-    acc = 0
-
-    return acc
-
 
 day = 8
 lin = [l.replace('\n', '') for l in util.get_input(day)]
 start = time.time()
 
-print('part 1: '+str(part1()))
-print('part 2: '+str(part2()))
+part1_2()
+
 end = time.time()
 print(end - start)
