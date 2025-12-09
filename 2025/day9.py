6837e5496974c9c4ed4daece3443b30be14730ca
Michal Gabriel <mgabriel@artex-is.cz>
2025-12-09 18:21:29 +0100

    day9 part 2 - nějaké změny ale pořád to nefunguje

--- 2025/day9.py
+++ 2025/day9.py
@@ -28,17 +28,34 @@ def test_rect(p1, p2, pointlist) -> bool:
         p1, p2 = p2, p1
     if (p1[0] == p2[0]):
         step = 1 if (p1[1] < p2[1]) else -1
+        outside_dir = 1 if (not((p1[0]+1, p1[1]))in pointlist) else -1
+        inside = False
         for i in range(p1[1], p2[1]+step, step):
-            if (not ((p1[0], i) in pointlist)) and (not ((p1[0]-step, i-step) in pointlist)):
-                return False
+            if (not ((p1[0], i) in pointlist)):
+                if(not ((p1[0]-outside_dir, i-step) in pointlist)):
+                    if not inside:
+                        return False
+                else:
+                    inside = True
+            else:
+                inside = False
         return True
     if (p1[1] == p2[1]):
         step = 1 if (p1[0] < p2[0]) else -1
+        outside_dir = 1 if (not((p1[0], p1[1]+1))in pointlist) else -1
+        inside = False
         for i in range(p1[0], p2[0]+step, step):
-            if (not ((i, p1[1]) in pointlist)) and (not ((i-step, p1[1]+step) in pointlist)):
-                return False
+            if (not ((i, p1[1]) in pointlist)):
+                if (not ((i-step, p1[1]+outside_dir) in pointlist)):
+                    if not inside:
+                        return False
+                else:
+                    inside = True
+            else:
+                inside = False
         return True
-    points = [(p1[0], p1[1]), (p2[0], p1[1]), (p2[0], p2[1]), (p1[0], p2[1])]
+    points = collections.deque([(p1[0], p1[1]), (p2[0], p1[1]), (p2[0], p2[1]), (p1[0], p2[1])])
+    points.rotate(-points.index(min(points))-1)
     for i in range(-1, 3):
         Ok = True
         Ok = Ok and test_rect(points[i], points[i+1], pointlist)
@@ -66,18 +83,18 @@ def part2():
                 if i == lin[l][0]-step:
                     last = (i+step, last[1])
 
-    if lin[l][0] == last[0]:
-        step = 1 if (lin[l][1] > last[1]) else -1
-        for i in range(last[1], lin[l][1], step):
+    if lin[0][0] == last[0]:
+        step = 1 if (lin[0][1] > last[1]) else -1
+        for i in range(last[1], lin[0][1], step):
             pointlist.add((last[0], i+step))
-            if i == lin[l][1]-step:
+            if i == lin[0][1]-step:
                 last = (last[0], i+step)
 
-    if lin[l][1] == last[1]:
-        step = 1 if (lin[l][0] > last[0]) else -1
-        for i in range(last[0], lin[l][0], step):
+    if lin[0][1] == last[1]:
+        step = 1 if (lin[0][0] > last[0]) else -1
+        for i in range(last[0], lin[0][0], step):
             pointlist.add((i+step, last[1]))
-            if i == lin[l][0]-step:
+            if i == lin[0][0]-step:
                 last = (i+step, last[1])
 
     max_size = 0
