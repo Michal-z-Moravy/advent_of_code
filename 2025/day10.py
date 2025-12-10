fd0e1a989827efd2df0c162596ea7a480f21221a
Michal Gabriel <mgabriel@artex-is.cz>
2025-12-10 12:24:30 +0100

    2025 day 10 part 1 fix (probably not generic enough)

--- 2025/day10.py
+++ 2025/day10.py
@@ -22,28 +22,26 @@ def merge_actions(a1, a2) -> tuple:
 
 
 def process_line(i, target) -> int:
-    j = 1
     tmp_actions = set(action_lists[i])
+    tmp_action_costs = dict.fromkeys(tmp_actions, 1)
     while True:
         if target in tmp_actions:
-            return j
-        new_actions = set()
+            return tmp_action_costs[target]
         tmp_actions_L = list(tmp_actions)
         for x in range(len(tmp_actions_L)):
             for y in range(x+1, len(tmp_actions_L)):
-                new_actions.add(merge_actions(
-                    tmp_actions_L[x], tmp_actions_L[y]))
-        tmp_actions = tmp_actions.union(new_actions)
-        j += 1
+                action = merge_actions(tmp_actions_L[x], tmp_actions_L[y])
+                cost = tmp_action_costs[tmp_actions_L[x]
+                                        ]+tmp_action_costs[tmp_actions_L[y]]
+                tmp_actions.add(action)
+                if (not (action in tmp_action_costs)) or (tmp_action_costs[action] > cost):
+                    tmp_action_costs[action] = cost
 
 
 def part1():
     acc = 0
     for i in range(len(targets)):
-        tmp = process_line(i, targets[i])
-        out.write('steps: '+str(tmp)+' target: ' +
-                  str(targets[i])+' input:'+lin[i]+'\n')
-        acc += tmp
+        acc += process_line(i, targets[i])
     return acc
 
 
@@ -55,7 +53,6 @@ def part2():
 lin = [l.replace('\n', '') for l in util.get_input(2025, 10)]
 start = time.time()
 
-out = open('output_day10', 'w')
 targets = []
 action_lists = []
 joltages = []
@@ -74,5 +71,4 @@ for l in lin:
 print('part 1: '+str(part1()))
 print('part 2: '+str(part2()))
 end = time.time()
-out.close()
 print(end - start)
